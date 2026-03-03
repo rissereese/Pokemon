708ea613ca5bc2c0fa356e0b6eb353ddf34b36f8
rissereese <charissesan0875@gmail.com>
2026-03-03 11:15:17 -0500

    updates: fixed adding to team button
    Next updates: incorporate pokemone moves and fix the team layout

--- Pokemon_JS.js
+++ Pokemon_JS.js
@@ -1,20 +1,29 @@
+let input = null;
+let pokemonAPIurl = null;
 let pokemon = null;
 const imgArr = ["teamImage1", "teamImage2", "teamImage3", "teamImage4"];
+const selectedMoves = ["select1", "select2", "select3", "select4"];
+const lists = ["list1", "list2", "list3", "list4"];
 
 async function loadPokemonInfo(input, pokemonAPIurl) {
     let response = await fetch(pokemonAPIurl);
+    if (!response.ok) {
+        console.log("HTTP Error:", response.status);
+        return;
+    }
     let data = await response.json();
-    pokemon = data;
+    return data;
 }
 
-async function findPokemonInfo() {
-    let input = document.getElementById("pokemonInput").value.trim().toLowerCase();
-    const pokemonAPIurl = 'https://pokeapi.co/api/v2/pokemon/' + input;
-    await loadPokemonInfo(input, pokemonAPIurl);
+async function findPokemonInfo(input) {
+    input = document.getElementById("pokemonInput").value.trim().toLowerCase();
+    pokemonAPIurl = 'https://pokeapi.co/api/v2/pokemon/' + input;
+    pokemon = await loadPokemonInfo(input, pokemonAPIurl);
     displayPokemon(pokemon);
     pokemonSound(pokemon);
-    pokemonMoves(pokemon);
+    pokemonMovesOptionList(pokemon);
 }
+
 
 function displayPokemon(pokemon) {
     document.getElementById("pokemonImage").src = pokemon.sprites.front_default;
@@ -24,16 +33,51 @@ function pokemonSound(pokemon) {
     document.getElementById("pokemonAudio").src = pokemon.cries.latest;
 }
 
-function pokemonMoves(pokemon) {
-    
+function pokemonMovesOptionList(pokemon) {
+
 }
 
-function addToTeam(x) {
-    let i = 0;
-    let img = document.getElementById(imgArr[0]);
-    while (img.src == null || i > 4) {
-        i++;
-        img = document.getElementById(imgArr[i]);
+function findEmptyTeamSlot() {
+    let link = "https://img.freepik.com/free-vector/camouflage-pattern-texture-pixel-gray-shades-background_1017-30109.jpg?semt=ais_rp_progressive&w=740&q=80";
+    for (j = 0; j < 4; j++) {
+        x = document.getElementById(imgArr[j]);
+        if (x.src.includes(link)) {
+            return j;
+        }
     }
-    img = displayPokemon(x);
+    return -1;
+}
+
+function getPokemonMoves() {
+    let moves = [];
+    for (let i = 0; i < 4; i++) {
+        const move = document.getElementById(selectedMoves[i]).value;
+        moves.push(move);
+    }
+    return moves;
+}
+
+async function addToTeam() {
+    let j = 0;
+
+    console.log("Pokemon: ", pokemon);
+    j = findEmptyTeamSlot();
+    if (j === -1) {
+        console.log("Team is full.");
+    }
+
+    const img = document.getElementById(imgArr[j]);
+    img.style.display = "block";
+    img.src = pokemon.sprites.front_default;
+
+    const ul = document.getElementById(lists[j]);
+    ul.innerHTML = "";
+
+    const moves = getPokemonMoves();
+    moves.forEach(move => {
+        const li = document.createElement("li");
+        li.textContent = move;
+        ul.appendChild(li);
+    });
+    ul.style.display = "block";
 }
